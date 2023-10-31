fea98fe34d8ed59e98ee9ed39076c7c3886a71a7
Abhijit kumar <insphere.abhijit@gmail.com>
2023-10-31 23:57:19 +0530

    ui update in splash & login

---
+++ Constants/Mixins.js
@@ -0,0 +1,29 @@
+import {Dimensions, PixelRatio} from 'react-native';
+
+const WINDOW_WIDTH = Dimensions.get('window').width;
+const guidelineBaseWidth = 375;
+
+export const scaleSize = size => (WINDOW_WIDTH / guidelineBaseWidth) * size;
+export const scaleFont = size => size * PixelRatio.getFontScale();
+
+function dimensions(top, right = top, bottom = top, left = right, property) {
+  let styles = {};
+  styles[`${property}Top`] = top;
+  styles[`${property}Right`] = right;
+  styles[`${property}Bottom`] = bottom;
+  styles[`${property}Left`] = left;
+
+  return styles;
+}
+
+export function margin(top, right, bottom, left) {
+  return dimensions(top, right, bottom, left, 'margin');
+}
+
+export function padding(top, right, bottom, left) {
+  return dimensions(top, right, bottom, left, 'padding');
+}
+
+const FULL_SCREEN = Dimensions.get('screen');
+export const WIDTH = FULL_SCREEN.width;
+export const HEIGHT = FULL_SCREEN.height;
--- Constants/theme.js
+++ Constants/theme.js
@@ -1,22 +1,24 @@
 export const COLORS = {
-    primary: "#312651",
-    secondary: "#444262",
-    tertiary: "#FF7754",
-  
-    gray: "#83829A",
-    gray2: "#C1C0C8",
-  
-    white: "#F3F4F8",
-    lightWhite: "#FAFAFC",
-    white:"FFFFFE"
-  };
+  primary: "#312651",
+  secondary: "#444262",
+  tertiary: "#FF7754",
 
-  export const SIZES = {
-    xSmall: 10,
-    small: 12,
-    medium: 16,
-    large: 20,
-    xLarge: 24,
-    xxLarge: 32,
-  };
+  gray: "#83829A",
+  gray2: "#C1C0C8",
 
+  // white: "#F3F4F8",
+  lightWhite: "#FAFAFC",
+  white: "#FFFFFF",
+  white_light: "#FFFDF6",
+  pink: "#F39184",
+  yellow_shadow: "#F3D743",
+};
+
+export const SIZES = {
+  xSmall: 10,
+  small: 12,
+  medium: 16,
+  large: 20,
+  xLarge: 24,
+  xxLarge: 32,
+};
