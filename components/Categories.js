928ebb21588eadbdee05fe2ea34a51834549bd7a
Avinash-Mizzlecodes <aninashmizzlecodes@gmail.com>
2024-03-14 11:40:47 +0530

    Commit on categories section.

--- components/Categories.js
+++ components/Categories.js
@@ -34,7 +34,7 @@ const CategoriesTypes = () => {
     <View style={{marginTop:10}}>
      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
          {type.map((item, index)=>{
-           return( <TouchableOpacity onPress={() => navigation.navigate("MenScreen")} style={{marginHorizontal:2}} key={index}>
+           return( <TouchableOpacity onPress={() => navigation.navigate("MenScreen",{type:item.name})} style={{marginHorizontal:2}} key={index}>
             <Image source={item.image} />
                 <Text style={{textAlign:"center",fontSize:10,fontFamily:'regular', color:'#000'}}>{item.name}</Text>
             </TouchableOpacity>)
